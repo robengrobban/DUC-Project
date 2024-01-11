47c6bf56ea3b2fcee63590ce5cc43a26dc7a14af
Robert Englund <kastanjen3@gmail.com>
2024-01-11 13:55:46 +0100

    Update cpo.js

--- entities/cpo.js
+++ entities/cpo.js
@@ -70,13 +70,13 @@ class CPO extends Entity {
     generateRates() {
         let rates = [];
         for (let i = 0; i < this.rateSlots; i++) {
-            rates[i] = this.web3.utils.toBigInt( Math.floor(this.pricePerWattHoursToWattSeconds((-0.000001*i**2 + 0.0001*i + 0.0001)*this.precision)) );
-            /*if ( i % 2 == 0 ) {
+            //rates[i] = this.web3.utils.toBigInt( Math.floor(this.pricePerWattHoursToWattSeconds((-0.000001*i**2 + 0.0001*i + 0.0001)*this.precision)) );
+            if ( i % 2 == 0 ) {
                 rates[i] = this.web3.utils.toBigInt(Math.floor( (this.pricePerWattHoursToWattSeconds(0.001)*this.precision)+0.5 ));
             }
             else {
                 rates[i] = this.web3.utils.toBigInt(Math.floor( (this.pricePerWattHoursToWattSeconds(0.002)*this.precision)+0.5 ));
-            }*/
+            }
             //rates[i] = this.web3.utils.toBigInt(Math.floor( (this.pricePerWattHoursToWattSeconds(0.0005)*this.precision)+0.5 ));
         }
         return rates;
